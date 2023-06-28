dc65ca931eee699543c4e4bd362facfff3afa4ec
Laura Schu <contaclauraschu@gmail.com>
2023-06-28 00:47:58 -0300

    segunda parte dando tudo certo

--- teste_1.py
+++ teste_1.py
@@ -63,3 +63,68 @@ pygame.mixer.music.play(-1)
 stars = []
 lines = []
 
+# Função para desenhar as estrelas na tela
+def desenhar_estrelas():
+    for i, estrela in enumerate(stars):
+        pygame.draw.circle(tela, (255, 255, 255), estrela[1], 2)
+        texto = font.render(estrela[0], True, (255, 255, 255))
+        tela.blit(texto, (estrela[1][0] - 10, estrela[1][1] + 10))
+
+# Função para desenhar as linhas conectando as estrelas marcadas
+def desenhar_linhas():
+    for estrela1, estrela2 in lines:
+        x1, y1 = estrela1[1]
+        x2, y2 = estrela2[1]
+        x1 = round(x1)
+        y1 = round(y1)
+        x2 = round(x2)
+        y2 = round(y2)
+        pygame.draw.line(tela, (255, 255, 255), (x1, y1), (x2, y2), 1)
+        distancia_x = abs(x2 - x1)
+        distancia_y = abs(y2 - y1)
+        soma_distancias = distancia_x + distancia_y
+        texto_distancia = font.render(str(soma_distancias), True, (255, 255, 255))
+        posicao_texto = (
+            (x1 + x2) // 2,
+            (y1 + y2) // 2
+        )
+        tela.blit(texto_distancia, posicao_texto)
+
+# Função para exibir uma caixa de diálogo de entrada de texto e retornar o nome da estrela
+def obter_nome_estrela():
+    root = tk.Tk()
+    root.withdraw()
+    nome_estrela = simpledialog.askstring("Nome do Ponto", "Digite o nome do ponto:")
+    root.destroy()  # Fechar a janela de diálogo após obter o nome
+    return nome_estrela if nome_estrela else "Estrela Desconhecida"
+
+# Carregamento da fonte para exibição do nome das estrelas
+font = pygame.font.SysFont(None, 24)
+font_frases = pygame.font.SysFont('calibri', 18, italic=True)
+font_nomes = pygame.font.SysFont('calibri', 14)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            # Antes de fechar a aplicação, salvar as marcações
+            salvar_marcacoes()
+            pygame.quit()
+            sys.exit()
+        elif event.type == MOUSEBUTTONDOWN and event.button == 1:
+            # Obter o nome da estrela através de uma caixa de diálogo
+            nome_estrela = obter_nome_estrela()
+            # Obter a posição do clique do mouse
+            posicao_mouse = event.pos
+            # Adicionar o nome da estrela e a posição ao registro de marcações
+            estrela = (nome_estrela, posicao_mouse)
+            stars.append(estrela)
+
+            # Conectar as estrelas com linhas (apenas se houver mais de uma estrela marcada)
+            if len(stars) > 1:
+                estrela_anterior = stars[-2]
+                linha = (estrela_anterior, estrela)
+                lines.append(linha)
+
+    tela.blit(fundodetela, (0, 0))
+    desenhar_estrelas()
+    desenhar_linhas()
