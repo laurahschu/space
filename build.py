3a88d53a03e7110a0f937d2bd3b2bc06ec78602c
Julia Braccini <jubraccini2@gmail.com>
2023-06-28 01:34:32 -0300

    adicionando build

---
+++ build.py
@@ -0,0 +1,17 @@
+#pip install cx_freeze
+import cx_Freeze
+executables =[
+    cx_Freeze.Executable(script="main.py", icon="space.ico")
+]
+cx_Freeze.setup(
+    name = "Space Marker",
+    options={
+        "build_exe":{
+            "packages":["pygame"],
+            "include_files":["bgspace.jpg", "marcacoes.txt", "rocket.png", "spacesound.mp3"]
+        }
+    }, executables = executables
+)
+
+#py geraSetup.py build
+#py geraSetup.py bdist_msi
